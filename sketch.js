cb95336e3aed9252a52d4b600578d7edac1f16b8
Tatvi Shah <71923600+Tatvi@users.noreply.github.com>
2020-10-20 12:53:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var fRect, mRect
+
+function setup() {
+  createCanvas(1200,800);
+  fRect=createSprite(600,400,50,80);
+  fRect.shapeColor="green";
+  mRect=createSprite(400,200,80,30);
+  mRect.shapeColor="green";
+}
+
+function draw() {
+  background(255,255,255); 
+ mRect.x=World.mouseX
+ mRect.y=World.mouseY
+
+ if (mRect.x-fRect.x<fRect.width/2+mRect.width/2
+     && fRect.x-mRect.x<fRect.width/2+mRect.width/2
+     && mRect.y-fRect.y<fRect.height/2+mRect.height/2
+     && fRect.y-mRect.y<fRect.height/2+mRect.height/2) {
+       mRect.shapeColor="red"
+       fRect.shapeColor="red"    
+ } 
+ else {
+  mRect.shapeColor="green"
+  fRect.shapeColor="green"       
+ }
+ drawSprites();
+}
+
